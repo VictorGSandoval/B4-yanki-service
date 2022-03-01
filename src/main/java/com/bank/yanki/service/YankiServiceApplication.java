b7b8df28aed4e701d3b4f282aed31db9dc0b214e
Victor Sandoval <victor.sandoval@vallegrande.edu.pe>
2022-03-01 12:05:25 -0500

    Upload Projet of Eureka

--- src/main/java/com/bank/yanki/service/YankiServiceApplication.java
+++ src/main/java/com/bank/yanki/service/YankiServiceApplication.java
@@ -2,8 +2,10 @@ package com.bank.yanki.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class YankiServiceApplication {
 	public static void main(String[] args) {
